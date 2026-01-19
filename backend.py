1673437a98f201dc0ee8f817c6f4a483a8bdf001
Elias Emanuel Almada <124707045+EliasIchi@users.noreply.github.com>
2026-01-18 21:00:34 -0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -15,18 +15,26 @@ ultimo_pago = {
 @app.post("/webhook")
 async def webhook(request: Request):
     data = await request.json()
+    print("WEBHOOK:", data)  # 🔍 LOG
 
-    if data.get("type") == "payment":
-        pid = data["data"]["id"]
-        pago = sdk.payment().get(pid)["response"]
+    payment_id = None
+
+    # caso 1
+    if "data" in data and "id" in data["data"]:
+        payment_id = data["data"]["id"]
+
+    if payment_id:
+        pago = sdk.payment().get(payment_id)["response"]
 
         ultimo_pago["status"] = pago["status"]
-        ultimo_pago["payment_id"] = pid
+        ultimo_pago["payment_id"] = payment_id
         ultimo_pago["transaction_id"] = (
             pago.get("transaction_details", {})
                 .get("transaction_id")
         )
+
     return {"ok": True}
+
 
 @app.get("/estado")
 def estado():
