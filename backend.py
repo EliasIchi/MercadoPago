2703e8f0cc427851a38a0bd66104df05aedab61e
Elias Emanuel Almada <124707045+EliasIchi@users.noreply.github.com>
2026-01-19 23:51:09 -0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 import mercadopago
 import uuid
 
-MP_ACCESS_TOKEN = "APP_USR-1554767517582736-011922-6d5db8152ee5ca891ebd1594293be76d-145557542"
+MP_ACCESS_TOKEN = "APP_USR-5249994394996474-011214-398ad97c27842a3a4d81a9bd045f32fb-145557542"
 
 sdk = mercadopago.SDK(MP_ACCESS_TOKEN)
 app = FastAPI()
